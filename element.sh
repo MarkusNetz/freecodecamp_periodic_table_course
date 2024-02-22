f0c4b2fa45bba949c0dc1671bbbb42e9b99dc23a
Markus Netz <markus.netz.89@gmail.com>
2024-02-22 22:10:44 +0100

    fix: finish the element.sh script to accomodate freecode user stories.

--- element.sh
+++ element.sh
@@ -1,22 +1,31 @@
-#/usr/bin/bash
+#!/bin/bash
 
-if [[ "${#}" -lt 1 ]]; then
-  echo "Please provide an element as an argument."
-  exit 0
+#
+# Variables
+#
+
+
+readonly PSQL_CONNECT="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
+
+# Go through given input to script
+if [[ ${#} -eq 1 ]]; then
+  arg="${1}"
+  shift
+  if [[ $arg =~ ^[1-9]+$ ]]; then
+    atomic_number="$arg"
+    query_result=$(${PSQL_CONNECT} "SELECT 'The element with atomic number ' || atomic_number || ' is ' || name || ' (' || symbol || '). It´s a ' || type || ', with a mass of ' || atomic_mass || ' amu. ' || name || ' has a melting point of ' || melting_point_celsius || ' celsius and a boiling point of ' || boiling_point_celsius || ' celsius.' FROM elements INNER JOIN properties using(atomic_number) INNER JOIN types ON types.type_id = properties.type_id WHERE elements.atomic_number = ${atomic_number} ;" | grep "The element")
+  else
+    element="${arg}"
+    query_result=$(${PSQL_CONNECT} "SELECT 'The element with atomic number ' || atomic_number || ' is ' || name || ' (' || symbol || '). It´s a ' || type || ', with a mass of ' || atomic_mass || ' amu. ' || name || ' has a melting point of ' || melting_point_celsius || ' celsius and a boiling point of ' || boiling_point_celsius || ' celsius.' FROM elements INNER JOIN properties using(atomic_number) INNER JOIN types ON types.type_id = properties.type_id WHERE elements.symbol LIKE '${element}' OR elements.name LIKE '${element}' ;" | grep "The element")
+  fi
+
+  
+  if [[ -n "${query_result}" ]]; then
+    echo "${query_result}" | sed -e "s/´/'/" -e 's/^ //g'
+  else
+    echo "I could not find that element in the database."
+  fi
+elif [[ ${#} -eq 0 ]]; then
+  echo Please provide an element as an argument.
 fi
 
-element_to_find=$1
-shift
-
-query_reply=$(psql --username=freecodecamp \
-  --dbname=periodic_table \
-  -c "SELECT 'The element with atomic number ' || atomic_number || ' is ' || name || ' (' || symbol || '). It´s a ' || type || ', with a mass of ' || atomic_mass || ' amu. ' || name || ' has a melting point of ' || melting_point_celsius || ' celsius and a boiling point of ' || boiling_point_celsius || ' celsius.' FROM elements INNER JOIN properties using(atomic_number) INNER JOIN types ON types.type_id = properties.type_id WHERE(elements.symbol LIKE '%${element_to_find}%' OR elements.name LIKE '%${element_to_find}%' OR elements.atomic_number = ${element_to_find}) ;" | grep "The element")
-
-if [[ -z "${query_reply}" ]]; then
-  echo "I could not find that element in the database."
-  exit 1;
-else
-  echo "${query_reply}"
-fi
-
-exit 0
